de4d38331db80cab1d40f41a8db27514868989e7
mohmmedahmed00m-dot <mohmmedahmed00m@gmail.com>
2026-07-07 12:40:36 +0300

    Delete extracted_jvz-main-fixed/jvz-main directory

--- extracted_jvz-main-fixed/jvz-main/backend/src/common/crypto/crypto.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EncryptionService } from './encryption.service';
-import { HashService } from './hash.service';
-
-@Module({
-  providers: [EncryptionService, HashService],
-  exports: [EncryptionService, HashService],
-})
-export class CryptoModule {}
--- extracted_jvz-main-fixed/jvz-main/frontend/src/components/styles-import.ts
+++
@@ -1,4 +0,0 @@
-// Central style import so components only need one import line.
-import '../styles/tokens.css';
-import '../styles/components.css';
-import '../styles/app.css';
